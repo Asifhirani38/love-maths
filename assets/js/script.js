a13fd7c63663f44b997a035a86f1883ccd5fba3f
Asif Hirani <155246558+Asifhirani38@users.noreply.github.com>
2024-04-14 22:38:04 +0100

    added function for division which was the last remaining math operation on the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,10 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2);
   } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
-  } else if ((gameType = "subtract")) {
+  } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting`;
@@ -91,6 +93,8 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"];
   } else if (operator === "-") {
     return [operand1 - operand2, "subtraction"];
+  } else if (operator === "/") {
+    return [operand1 / operand2, "division"];
   } else {
     alert(`unimplemented operator ${operator}`);
     throw `unimplemented operator ${operator}. Aborting`;
@@ -137,4 +141,13 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
   document.getElementById("operator").textContent = "x";
+}
+
+//Function for division game operation
+
+function displayDivisionQuestion(operand1, operand2) {
+  operand1 = operand1 * operand2;
+  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById("operator").textContent = "/";
 }
